3778c23242c7d39d3a307dafacf5b180b2a10226
Jhon Muller <jmuller.jhon80@gmail.com>
2022-11-04 17:40:19 -0400

    feat: criacao de estrutura basica para a api

---
+++ src/app.js
@@ -0,0 +1,17 @@
+// const express = require('express');
+import express from "express"
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    return res.send('Hello, express!');
+});
+
+const port = 3000;
+
+app.listen(port, ()=>{
+    console.log(`App rodando na porta ${port}`);
+});
+
+
